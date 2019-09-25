56c4dbb731bd4f535621ca75ec8e8339a7736f13
qiufeihong <qiufeihong2018@126.com>
2019-09-25 18:08:13 +0800

    fix(解决pipelineState/search中getState方法未找到和新增pipelineStates数据crud的方法):

--- routers/pipelineState.js
+++ routers/pipelineState.js
@@ -198,4 +198,61 @@ router.post('/time', function(req, res) {
   });
 });
 
+// {
+// 	"_id" : ObjectId("5d89dee56d385020a05c0526"),
+// 	"state" : "off",
+// 	"startTime" : ISODate("2019-09-24T09:10:59.670Z"),
+// 	"endTime" : ISODate("2019-09-24T23:11:34.167Z"),
+// 	"difTime" : 50434497,
+// 	"count" : 8862,
+// 	"createdAt" : ISODate("2019-09-24T09:16:21.380Z"),
+// 	"updatedAt" : ISODate("2019-09-24T23:11:34.167Z"),
+// 	"__v" : 0
+// }
+// 2019-09-24T16:00:00.007Z
+// db.pipelinestates.findByIdAndUpdate({
+//   _id: '5d89dee56d385020a05c0526'
+// }, {
+//   $set: {
+//     endTime: '2019-09-24T16:00:00.007Z',
+//     difTime: '24541000'
+//   }
+// }, {
+//   new: true,
+//   upsert: true,
+//   setDefaultsOnInsert: true,
+//   setOnInsert: true
+// })
+router.put('/', function(req, res) {
+  const {
+    _id,
+    endTime,
+    difTime
+  } = req.body;
+  console.log(_id, endTime, difTime)
+  PipelineState.findByIdAndUpdate({
+    _id: _id
+  }, {
+    $set: {
+      endTime: endTime,
+      difTime: difTime
+    }
+  }, {
+    new: true,
+    upsert: true,
+    setDefaultsOnInsert: true,
+    setOnInsert: true
+  }, function(err, doc) {
+    log.info(doc);
+  });
+});
+
+router.post('/', function(req, res) {
+  const data = req.body;
+  // console.log(data)
+  PipelineState.create(data).then((doc) => {
+    log.info(doc)
+  })
+});
+
 module.exports = router;
